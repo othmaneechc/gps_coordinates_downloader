b097fd8f0dd4dc8cd940cc994a676992cdfc5fb1
othmaneechc <echchaabiothmane@gmail.com>
2024-07-10 18:16:46 +0800

    app

--- api/index.py
+++ api/index.py
@@ -1,11 +1,35 @@
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return 'Hello, World!'
+
+# @app.route('/about')
+# def about():
+#     return 'About'
+
+import csv
+import os
+
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/save_coords', methods=['POST'])
+def save_coords():
+    data = request.json
+    lat = data['lat']
+    lng = data['lng']
+    with open('coordinates.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([lat, lng])
+    return jsonify({'status': 'success'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
